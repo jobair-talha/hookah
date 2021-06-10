10a8ff8e9065506080d5d50f54f3de85fc650f2e
jobair-talha <talhajobair10@gmail.com>
2021-06-11 01:06:24 +0600

    mm

--- js/script.js
+++ js/script.js
@@ -125,7 +125,7 @@ $('.team-slider').slick({
   speed: 300,
   slidesToShow: 4,
   slidesToScroll: 1,
-  // autoplay: true,
+  autoplay: true,
   arrows: false,
   
 });
@@ -135,7 +135,49 @@ $('.gallery-slider').slick({
   speed: 300,
   slidesToShow: 5,
   slidesToScroll: 2,
-  // autoplay: true,
+  autoplay: true,
   arrows: false,
   
+});
+
+
+// Testimonial slider
+
+$('.testi-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: true,
+  prevArrow: '<i class="fas fa-angle-left prev"></i>',
+  nextArrow: '<i class="fas fa-angle-right next"></i>',
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
